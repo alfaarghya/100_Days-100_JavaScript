c79dc45e02868e7d4f41a303208ba3cd6af29f88
alfaArghya <arghya.coding@gmail.com>
2023-11-08 17:08:59 +0530

    change in generateRandomQuote function

--- 05-TypingTest/js/script.js
+++ 05-TypingTest/js/script.js
@@ -7,19 +7,23 @@ const generateRandomQuote = async () => {
         const response = await fetch("https://api.quotable.io/random?minLength=100&maxLength=200");
         let data = await response.json();
         quote = data.content;
-        quoteHTML.innerHTML = quote;
+        // quoteHTML.innerHTML = quote;
+        let arr = quote.split("").map((val) => {
+            return "<span class='quote-char'>" + val + "</span>";
+        });
+        quoteHTML.innerHTML = arr.join("");
+        // console.log(arr.join(""));
     } catch (error) {
         console.log(error);
-        quoteHTML.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem recusandae dicta ipsa autem dignissimos corporis repudiandae esse deserunt distinctio quo?";
+        quote = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem recusandae dicta ipsa autem dignissimos corporis repudiandae esse deserunt distinctio quo?"
+
+        let arr = quote.split("").map((val) => {
+            return "<span class='quote-char'>" + val + "</span>";
+        });
+
+        quoteHTML.innerHTML = arr.join("");
+        // console.log(arr.join(""));
     }
-
-
-
-
-    // let arr = quote.split("").map((val) => {
-    //     return "<span class='quote-char'>" + val + "</span>";
-    // });
-    // document.getElementById("quote").innerHTML = arr.join("");
 
 };
 
